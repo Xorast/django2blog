2b83beb9aa3748f406d64ee1583777659d69f75e
Xavier <xavier.astor.fontaine@gmail.com>
2018-06-18 13:38:49 +0000

    Added accounts app and implemented author field to posts

--- posts/forms.py
+++ posts/forms.py
@@ -3,5 +3,5 @@ from .models import Post
 
 class BlogPostForm(forms.ModelForm):
     class Meta:
-        model = Post
+        model  = Post
         fields = ('title', 'content', 'image', 'tag', 'published_date')
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.utils import timezone
-from .models import Post
-from .forms import BlogPostForm
+from django.shortcuts   import render, get_object_or_404, redirect
+from django.utils       import timezone
+from .models            import Post
+from .forms             import BlogPostForm
 
 def get_posts(request):
     posts = Post.objects.all()
@@ -25,13 +25,31 @@ def create_or_edit_post(request, pk=None):
     Create a view that allows us to create
     or edit a post depending if the Post ID
     is null or not
+    
+    The first line (post = ...) if to take already existing post, 
+    to edit it and showing all the data for that post.
     """
     post = get_object_or_404(Post, pk=pk) if pk else None
+    
     if request.method == "POST":
+        
         form = BlogPostForm(request.POST, request.FILES, instance=post)
+        
         if form.is_valid():
-            post = form.save()
+            
+            post        = form.save(commit=False)
+            """
+            To keep the same author if the admin want to edit,
+            if no author ==> first creation, take the user as author
+            if author    ==> we keep it (no action on it)
+            """
+            if not post.author : 
+                post.author = request.user
+            post.save()
+            
             return redirect('post_detail', post.pk)
+    
     else:
         form = BlogPostForm(instance=post)
+    
     return render(request, 'posts/blogpostform.html', {'form': form})
